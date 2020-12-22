a417071dfbcc0a97d9c3805738c1aeb1d714ac6c
Brad Chiappetta <brad@greynoise.io>
2020-12-22 14:59:52 -0500

    add configuration file and update ability for local api key

--- transforms/GreyNoiseIPNoiseLookup.py
+++ transforms/GreyNoiseIPNoiseLookup.py
@@ -8,17 +8,22 @@ class GreyNoiseIPNoiseLookup(DiscoverableTransform):
 
     @classmethod
     def create_entities(cls, request, response):
+        if 'GNAPiKey' in request.TransformSettings:
+            api_key = request.TransformSettings['GNApiKey']
+        else:
+            # enter API key here for local transform
+            api_key = ''
         headers = {'Accept': 'application/json',
-                   'key': request.TransformSettings['GNApiKey']}
+                   'key': api_key}
         resp = requests.get('https://api.greynoise.io/v2/noise/quick/{0}'.format(
             request.Value), params={}, headers=headers)
         if resp.status_code == 200:
             g = resp.json()
             if g['noise']:
                 response.addEntity(
-                    'csr.greynoiseclassification', 'Noise Detected')
+                    'greynoise.noise', 'Noise Detected')
             else:
-                response.addEntity('csr.greynoiseclassification',
+                response.addEntity('greynoise.noise',
                                    'No Noise Detected')
         else:
             response.addUIMessage(
