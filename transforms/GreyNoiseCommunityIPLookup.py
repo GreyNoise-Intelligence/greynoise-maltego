d0c387ba75caeafac6b49beb6cdff9f7a4fba6dc
Philipp Dowling <phdowling@users.noreply.github.com>
2021-04-07 17:30:55 +0200

    Update GreyNoiseCommunityIPLookup.py

--- transforms/GreyNoiseCommunityIPLookup.py
+++ transforms/GreyNoiseCommunityIPLookup.py
@@ -1,6 +1,26 @@
 from greynoise import GreyNoise
-from maltego_trx.entities import Person
+from maltego_trx.overlays import OverlayPosition, OverlayType
+from maltego_trx.maltego import MaltegoEntity
 from maltego_trx.transform import DiscoverableTransform
+
+
+def add_display_info(ip_ent: MaltegoEntity, classification, last_seen, link):
+    link_text = "" if not link else f'<h3><a href="{link}">Open in GreyNoise</a></h3> <br/>'
+    classification_text = "" if not classification else f"GreyNoise classification for IP: {classification}<br/>"
+    last_seen_text = "" if not last_seen else f"Last seen by GreyNoise: {last_seen}"
+    ip_ent.addDisplayInformation(
+        f"{link_text}{classification_text}{last_seen_text}",
+        "GreyNoise Community"
+    )
+    colour = None
+    if classification == "benign":
+        colour = "green"
+    elif classification == "malicious":
+        colour = "red"
+
+    if colour:
+        ip_ent.addProperty(fieldName="gn_color", displayName="GreyNoise color", value=color, matchingRule="loose")
+        ip_ent.addOverlay(propertyName="gn_color", position=OverlayPosition.NORTH_WEST, overlayType=OverlayType.COLOUR)
 
 
 class GreyNoiseCommunityIPLookup(DiscoverableTransform):
@@ -12,22 +32,28 @@ class GreyNoiseCommunityIPLookup(DiscoverableTransform):
             integration_name="maltego-community-v1.0.0",
             offering="community",
         )
+        input_ip = response.addEntity("maltego.IPv4Address", request.Value)
         try:
             resp = api_client.ip(request.Value)
-            if resp["noise"]:
-                response.addEntity("greynoise.noise", "Noise Detected")
-                response.addEntity(Person, resp["name"])
-                response.addEntity("greynoise.classification", resp["classification"])
-                response.addEntity("greynoise.last_seen", resp["last_seen"])
-                response.addEntity("greynoise.link", resp["link"])
-            elif resp["riot"]:
+            if resp["noise"] or resp["riot"]:
+                if resp["noise"]:
+                    response.addEntity("greynoise.noise", "Noise Detected")
                 if resp["riot"]:
                     response.addEntity("greynoise.riot", "Benign Service Detected")
-                    response.addEntity(Person, resp["name"])
-                    response.addEntity("greynoise.last_seen", resp["last_seen"])
-                    response.addEntity("greynoise.link", resp["link"])
+                response.addEntity("maltego.Alias", resp["name"])
+                response.addEntity("greynoise.classification", resp["classification"])
+                response.addEntity("maltego.DateTime", resp["last_seen"])
+                url = response.addEntity("maltego.URL", resp["link"])
+                url.addProperty(
+                    fieldName="short-title", displayName="GreyNoise color", value=resp["link"], matchingRule="strict"
+                )
+                url.addProperty(
+                    fieldName="url", displayName="GreyNoise color", value=resp["link"], matchingRule="strict"
+                )
             else:
                 response.addEntity("greynoise.noise", "No Noise Detected")
-                response.addUIMessage("This IP address hasn't been seen by GreyNoise")
+                response.addUIMessage(f"The IP address {request.Value} hasn't been seen by GreyNoise.")
+
+            add_display_info(input_ip, resp.get("classification"), resp.get("last_seen"), resp.get("link"))
         except Exception as e:
             response.addUIMessage(e)
