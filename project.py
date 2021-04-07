cbc0d5793228f550136f14c7eefa60e168e690eb
Philipp Dowling <phdowling@users.noreply.github.com>
2021-04-07 17:32:38 +0200

    Update project.py

--- project.py
+++ project.py
@@ -5,6 +5,10 @@ from maltego_trx.registry import register_transform_function, register_transform
 from maltego_trx.server import app, application
 from maltego_trx.handler import handle_run
 
+import maltego_trx.maltego
+# hotfix until adjusted in upstream
+maltego_trx.maltego.DISP_INFO_TEMPLATE = "<Label Name=\"%(name)s\" Type=\"text/html\"><![CDATA[%(content)s]]></Label>"
+
 register_transform_classes(transforms)
 
 handle_run(__name__, sys.argv, app)
