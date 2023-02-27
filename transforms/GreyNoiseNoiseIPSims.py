8066f4d6aa66d062249c650d080285ddf36724e3
Brad Chiappetta <brad@greynoise.io>
2023-02-27 09:37:42 -0500

    add note section to ip sim and pre-commit to repo

--- transforms/GreyNoiseNoiseIPSims.py
+++ transforms/GreyNoiseNoiseIPSims.py
@@ -1,8 +1,22 @@
 from greynoise import GreyNoise
-from maltego_trx.maltego import MaltegoMsg
+from maltego_trx.maltego import MaltegoEntity, MaltegoMsg
 from maltego_trx.transform import DiscoverableTransform
 
 from .utility import INTEGRATION_NAME
+
+
+def add_display_info(ip_ent: MaltegoEntity, similar_ips, ip_address):
+    link_text = (
+        f'<h3><a href="https://viz.greynoise.io/ip-similarity/{ip_address}">'
+        f"See Similarity results in GreyNoise</a></h3><br/>"
+    )
+
+    similarity_text = f"IP address {ip_address} is similar to {similar_ips}" f" in the GreyNoise data set<br/>"
+
+    ip_ent.addDisplayInformation(
+        f"{link_text}{similarity_text}",
+        "GreyNoise Similarity",
+    )
 
 
 class GreyNoiseNoiseIPSims(DiscoverableTransform):
@@ -36,8 +50,15 @@ class GreyNoiseNoiseIPSims(DiscoverableTransform):
         try:
             resp = api_client.similar(request.Value, min_score=minimum_score, limit=limit)
             if "similar_ips" in resp:
+                similar_ips = len(resp["similar_ips"])
                 for item in resp["similar_ips"]:
                     response.addEntity("maltego.IPv4Address", item["ip"])
+
+                add_display_info(
+                    input_ip,
+                    similar_ips,
+                    resp.get("ip"),
+                )
 
             else:
                 response.addUIMessage(f"The IP address {request.Value} has no similar IPs within GreyNoise.")
